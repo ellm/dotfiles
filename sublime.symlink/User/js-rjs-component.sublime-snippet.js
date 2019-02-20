9e7ca73d7f4d1a58594914e32b7cd1bf8dad15b7
Matthew Ell <matt.ell@alley.co>
2019-02-20 12:02:26 -0500

    sublime updates

---
+++ sublime.symlink/User/js-rjs-component.sublime-snippet.js
@@ -0,0 +1,20 @@
+<snippet>
+    <content><![CDATA[
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * $1 component.
+ */
+export default class $2 extends Component {
+  static propTypes = {};
+
+  render() {$3}
+}
+});
+]]></content>
+    <!-- Optional: Set a tabTrigger to define how to trigger the snippet -->
+    <tabTrigger>rjscomp</tabTrigger>
+    <!-- Optional: Set a scope to limit where the snippet will trigger -->
+    <scope>source.js</scope>
+</snippet>
